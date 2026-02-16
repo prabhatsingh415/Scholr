3208c422d83d62a0303888c3e1945b8794f8c471
Prabhat Singh <prabhatsinghrajput795@gmail.com>
2026-02-16 15:19:26 +0530

    first commit

---
+++ JavaService/src/main/java/com/Scholr/Scholr/ScholrApplication.java
@@ -0,0 +1,13 @@
+package com.Scholr.Scholr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholrApplication.class, args);
+	}
+
+}
---
+++ JavaService/src/test/java/com/Scholr/Scholr/ScholrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Scholr.Scholr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
